22c22ea754459678b3a5c7bb47d9f93cccafa4a5
nevfeldeniz <nevfeldenizz@gmail.com>
2026-06-30 04:43:02 +0300

    Create HomePage.jsx

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,27 @@
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import About from '../components/About'
+import Amenities from '../components/Amenities'
+import Rooms from '../components/Rooms'
+import RoomShowcase from '../components/RoomShowcase'
+import Gallery from '../components/Gallery'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
+
+export default function HomePage() {
+  return (
+    <div className="min-h-screen bg-cream text-ink">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Amenities />
+        <Rooms />
+        <RoomShowcase />
+        <Gallery />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  )
+}
